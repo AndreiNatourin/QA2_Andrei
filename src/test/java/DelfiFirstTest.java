c2bde09732735886dfab315e71210216d46cc0e0
AndreiNatourin <art.i.g@inbox.lv>
2019-09-06 18:05:52 +0300

    ja andrei

---
+++ src/test/java/DelfiFirstTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DelfiFirstTest {
+
+    @Test
+    public void delfiFirstTest () {
+        // Here we will write a code
+        System.setProperty("webdriver.chrome.driver", "c:/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://delfi.lv");
+
+    }
+
+
+
+}
