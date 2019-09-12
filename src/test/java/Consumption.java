9c36b8a9ac64aa39da822fc128ecb7782f8b86e3
AndreiNatourin <art.i.g@inbox.lv>
2019-09-12 18:41:01 +0300

    ja andrei

---
+++ src/test/java/Consumption.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Consumption {
+    @Test
+    public double calculateConsumption (double a, double b) {
+        // a - tank volume
+        // b - distance
+        double c; // c - fuel consumption
+        c = (a*100.0f)/b;
+        return c;
+
+    }
+    @Test
+    public void showConsumption () {
+        double d; // new consumption d
+        double e; // new consumption e
+        d = (50*100.0f)/500;
+        e = (40*100.0f)/800;
+        System.out.println("d is: "+ d +" ltr/100km and e is: "+ e + " ltr/100km");
+
+        Assertions.assertEquals(10, d);
+        Assertions.assertEquals(5, e);
+    }
+}
